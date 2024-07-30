5f755ae0f37a3aa53425c71468127cf14dd9f4a6
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2024-07-30 23:08:57 +0700

    init Express webSocket

--- index.js
+++ index.js
@@ -5,44 +5,47 @@
 // node index.js
 
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
 const port = 3000;
+
+// Tạo HTTP server
+const server = http.createServer(app);
+
+// Tạo WebSocket server
+const wss = new WebSocket.Server({ server });
 
 // Middleware để xử lý JSON
 app.use(express.json());
 
-// Endpoint GET
+// Endpoint GET cơ bản
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Endpoint POST
-app.post('/data', (req, res) => {
-  const data = req.body;
-  res.send(`You sent: ${JSON.stringify(data)}`);
+// Xử lý kết nối WebSocket
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  // Gửi thông điệp khi kết nối được thiết lập
+  ws.send('Welcome to the WebSocket server!');
+
+  // Xử lý thông điệp từ client
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    // Gửi lại thông điệp cho client
+    ws.send(`You sent: ${message}`);
+  });
+
+  // Xử lý khi client ngắt kết nối
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
 
-// Endpoint GET với tham số URL
-app.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`User ID: ${userId}`);
-});
-
-// Bắt đầu server
-app.listen(port, () => {
+// Bắt đầu HTTP và WebSocket server
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
-
-
-
-// Middleware để log các yêu cầu
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-// Middleware để xử lý lỗi
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
 });
